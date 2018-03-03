f5b2c88023ee7894ee447921c6b3d4168c9f7110
Ricardo <rickysilvamoreno@gmail.com>
2018-03-03 12:42:17 -0600

    Add project

---
+++ Assets/Scripts/GndRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GndRotation : MonoBehaviour {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        if(Time.fixedTime < 30) {
+            transform.Rotate(new Vector3(45, 0, 0) * Time.deltaTime / 5);
+        }
+        else if(Time.fixedTime >30 && Time.fixedTime <60)
+        {
+            transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime / 5);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(45, 45, 0) * Time.deltaTime / 5);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public float jumpForce;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        rb.AddForce (movement * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+       
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+            RpcRespawn();
+        }
+    }
+
+    void RpcRespawn()
+    {
+            // move back to zero location
+            transform.position = new Vector3(0,50,0);
+    }
+}
